37dfce90c368b472d573298ad4b0be527e7d5531
Muhammad Adil <m.angillath@student.saedubai.com>
2022-06-21 11:48:20 +0400

    character_stats

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from tkinter import Variable
+
+
+d1='pee_pee'
+health=9999
+d2=350
+st=200
+special_move=500
+print("name:" + str(d1))
+print("health:" + str(health))
+print("damage:" + str(d2))
+print("stamina" + str(st))
