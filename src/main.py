5460796f24d599074d6d5a84951994f0db0944a4
Muhammad Adil <m.angillath@student.saedubai.com>
2022-06-21 12:40:52 +0400

    loops and defining function
    added an array that shows the sum

--- src/main.py
+++ src/main.py
@@ -10,3 +10,12 @@ print("name:" + str(d1))
 print("health:" + str(health))
 print("damage:" + str(d2))
 print("stamina" + str(st))
+enemyhp=[30,"💪",29,50,60,70,50]
+print(enemyhp[1])
+
+
+
+sum=0
+for i in range(0,5):
+    sum+=enemyhp[i]
+    print(sum)
