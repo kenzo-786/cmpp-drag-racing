ab17da64ab38472c8fe2851356481d0256940d16
Muhammad Adil <m.angillath@student.saedubai.com>
2022-07-05 10:50:18 +0400

    racing turtle
    the code is to race a turtle in yellow colour

---
+++ src/knzz.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(900,900)
+
+my_turtle = Turtle("turtle")
+my_turtle.color("yellow")
+my_turtle.goto(700,0)
+
+
+screen.exitonclick()
+
